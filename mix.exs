25053b9a43b31f73b2a3aaf4bfac922c0664d919
Mario A Chavez <mario.chavez@gmail.com>
2015-09-17 16:12:19 -0500

    Update dependencies and version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Dash.Mixfile do
 
   def project do
     [app: :dash,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
