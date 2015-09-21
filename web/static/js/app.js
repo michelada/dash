db3c1c9bdfc797bdf98a3593717583e15a1f0e2b
Mario A Chavez <mario.chavez@gmail.com>
2015-09-21 12:52:53 -0500

    Twitter popup

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -24,4 +24,21 @@ import {Menu} from "./menu"
 
 $(function() {
   var menu = new Menu('.home-menu', '.menu-toggle');
+
+  $('.share').click(function(event) {
+    var width  = 575,
+    height = 400,
+    left   = ($(window).width()  - width)  / 2,
+    top    = ($(window).height() - height) / 2,
+    url    = this.href,
+    opts   = 'status=1' +
+      ',width='  + width  +
+      ',height=' + height +
+      ',top='    + top    +
+      ',left='   + left;
+
+    window.open(url, 'twitter', opts);
+
+    return false;
+  });
 });
