fcb3d9f553038df8477a73f6ccd5e9ae6aff0bcf
Mario Alberto Chávez <mario.chavez@gmail.com>
2015-12-03 12:23:10 -0600

    Update footer and body font

--- lib/locales/en.ex
+++ lib/locales/en.ex
@@ -3,7 +3,7 @@ defmodule I18n do
 
   locale "en", [
     seo: [
-      title: "Design, develop, and launch awesome products",
+      title: "We plan, code and launch awesome web and mobile products.",
       description: "We work with startups and established companies implementing great ideas with simplicity and code quality",
       keywords: "design, develop, startup, code, ruby, rails, node, javascript, elixir, angularjs, reactjs, ios, android, app, legacy, maintenance, upgrade"
     ]
