380b16b8cb21d4a20ca1375c4ed951bdc6aeeebe
Mario Alberto Chávez <mario.chavez@gmail.com>
2017-03-17 16:31:20 -0600

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -30,13 +30,13 @@ defmodule Dash.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.2.3"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
+     {:gettext, "~> 0.13.1"},
      {:earmark, "~>1.1"},
      {:distillery, "~> 1.0"},
      {:chronos, "~> 1.7.0"},
