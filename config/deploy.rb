c29188088ec5a424bcc8456053b88bc88be4ecae
Mario Alberto Chávez <mario.chavez@gmail.com>
2018-03-06 19:51:03 -0600

    Update Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
 # config valid only for current version of Capistrano
-lock '3.10.1'
+lock '3.8.0'
 
 set :application, 'michelada.io-blog'
 set :repo_url, 'git@github.com:Michelada/dash.git'
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :default_env, mix_env: 'prod', HOST: 'blog.michelada.io', PUBLIC_PORT: 80, SCHEME: 'http', PORT: 4002, ADMIN_PATH: 'yXAv41YW5uyUDvd47tPa', DB_URL: 'ecto://dash:dash_manager@localhost/dash_prod'
+set :default_env, mix_env: 'prod', HOST: 'blog.michelada.io', PUBLIC_PORT: 443, SCHEME: 'https', PORT: 4002, ADMIN_PATH: ENV['ADMIN_PATH'], DATABASE_URL: ENV['DATABASE_URL']
 set :deploy_to, '/home/deploy/www/dash-blog'
 set :log_level, :debug
 set :keep_releases, 2
 
 set :linked_files, fetch(:linked_files, []).push('config/prod.secret.exs')
 set :linked_dirs, fetch(:linked_dirs, [])
-  .push('deps', 'node_modules', 'rel', '_build', 'priv/static/css', 'priv/static/js')
+  .push('deps', 'assets/node_modules', 'priv/static')
 
 task :dependencies do
   on roles(:web) do |_host|
@@ -27,7 +27,7 @@ end
 namespace :node do
   task :dependencies do
     on roles(:web) do |_host|
-      within(current_path) do
+      within("#{current_path}/assets") do
         execute(:npm, 'install')
       end
     end
@@ -37,7 +37,7 @@ end
 task :build do
   on roles(:web) do |_host|
     within(current_path) do
-      execute(:mix, 'release')
+      execute(:mix, 'release', '--env=prod')
     end
   end
 end
@@ -52,9 +52,11 @@ end
 
 task :assets do
   on roles(:web) do |_host|
+    within("#{current_path}/assets") do
+      execute('node_modules/brunch/bin/brunch', 'build', '--production')
+    end
     within(current_path) do
-      execute(:brunch, 'build', '--production')
-      execute(:mix, 'phoenix.digest')
+      execute(:mix, 'phx.digest')
     end
   end
 end
@@ -69,7 +71,7 @@ end
 
 task :start do
   on roles(:web) do |_host|
-    within("#{current_path}/rel/dash/bin") do
+    within("#{current_path}/_build/prod/rel/dash/bin") do
       execute('./dash', 'start')
     end
   end
@@ -77,7 +79,7 @@ end
 
 task :stop do
   on roles(:web) do |_host|
-    within("#{current_path}/rel/dash/bin") do
+    within("#{current_path}/_build/prod/rel/dash/bin") do
       execute('./dash', 'stop')
     end
   end
@@ -94,4 +96,4 @@ before :build, :dependencies
 before :build, 'node:dependencies'
 before :build, :assets
 after 'deploy:published', :build
-# after "deploy:published", :restart
+after "deploy:published", :restart
